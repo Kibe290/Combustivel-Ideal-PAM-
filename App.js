8fc0d902efe2abe1a97319e950141bbd4c1b4048
SAMARA RODRIGUES DE SOUSA <samara.sousa25@etec.sp.gov.br>
2026-05-21 12:45:45 -0300

    liripu

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Combustível Ideal, seu app que te ajuda a escolher o melhor preço </Text>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eaeaea',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt
 });
+// oq 
